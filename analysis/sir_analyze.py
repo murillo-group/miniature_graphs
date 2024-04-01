b8bb5b6b318afbf0f8fc51c602f4a9b50111a35c
DavidButts <davidbutts96@gmail.com>
2024-04-01 16:13:51 -0400

    change name

---
+++ analysis/sir_analyze.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'''
+@author: buttsdav@msu.edu
+last updated April 2024
+
+Individual's states can have the following values:
+0 : susceptible
+1 : infected
+2 : recovered
+
+to run:
+
+python .py example_graph/ graph.npz
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+import sys
+import os
+
+directory_name = sys.argv[1]
+# graph_name = sys.argv[2]
+#
+# make directory if it hasn't been made yet
+if not os.path.exists('./Results/'+directory_name+'SIR/'):
+    os.makedirs('./Results/'+directory_name+'SIR/',exist_ok=True)
+
+print(glob.glob('../simulations/Results/'+directory_name+'/SIR/*'))
+
+for file in glob.glob('../simulations/Results/'+directory_name+'/SIR/*'):
+    arr = np.load(file)
+    plt.plot(arr[:,0],c='C0',alpha=.5)
+    plt.plot(arr[:,1],c='C1',alpha=.5)
+    plt.plot(arr[:,2],c='C2',alpha=.5)
+plt.show()
+
+# print(arr)
+
+# arr = np.load('../simulations/Results/example_graph/graph.npz_SIR/run_2024_04_01_12_24_16_240273.npy')
+
+
+# plt.show()
