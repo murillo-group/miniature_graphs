ee82679742efe12647bb6ef3bff72c1556d17769
DavidButts <davidbutts96@gmail.com>
2024-04-02 14:10:08 -0400

    Start to batch file to run simulations and analysis

---
+++ batch/run_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run sir simulation
+echo python sir_main.py example_graph/ graph.npz
+
+python ../simulations/sir_main.py example_graph/ graph.npz
+
+echo python dk_main.py example_graph/ graph.npz
+
+python ../simulations/dk_main.py example_graph/ graph.npz
+
+echo python sir_analyze.py example_graph/ graph.npz
+
+python ../analysis/sir_analyze.py example_graph/ graph.npz
+
+echo python sir_
