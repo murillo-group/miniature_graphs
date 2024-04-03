a05bbf594aadbeed1000aae7ebda61772f4f74e0
DavidButts <davidbutts96@gmail.com>
2024-04-03 16:27:37 -0400

    adding in mini code and read commands

--- batch/run_all.sh
+++ batch/run_all.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# miniaturize a graph
+echo mpiexec -n 2 python ../miniaturize/parallel_tempering_main.py example_graph/ graph.npz data.txt
+
+read -rsn1 -p"Press any key to continue";echo
+
+mpiexec -n 2 python ../miniaturize/parallel_tempering_main.py example_graph/ graph.npz data.txt
+
 # Run sir simulation
 echo python sir_main.py example_graph/ graph.npz
 
