6423ea4242a5dd0a2d7de046e40b8c606f752995
JAMO2001 <jorge_martinez3@baylor.edu>
2024-04-10 16:25:58 -0400

    Updated data environment variable

--- miniaturize/__init__.py
+++ miniaturize/__init__.py
@@ -4,6 +4,7 @@ Miniaturize
 
 Python package to miniaturize large networks using the Metropolis-Hastings Algorithm
 """
+import os
 
 # Import Metropolis class created by David
 from miniaturize.Metropolis import *
@@ -13,4 +14,6 @@ from miniaturize.utils import *
 
 from importlib.util import find_spec
 
+# Set environment variables
 MINIATURIZE_DIR=find_spec('miniaturize').submodule_search_locations[0]
+DATA_DIR=os.path.join(os.path.split(MINIATURIZE_DIR)[0],"data")
