67f5421f2e3bd92928c09355e40d528d17263316
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-04-16 21:19:12 -0400

    Initial implementation of io module

--- miniaturize/viz/__init__.py
+++ miniaturize/viz/__init__.py
@@ -5,8 +5,3 @@ import os
 
 from miniaturize import MINIATURIZE_DIR
 from miniaturize.viz.viz import * 
-
-# Environment variables
-STYLE_SHEET_DIR=os.path.join(MINIATURIZE_DIR,"viz","sheets")
-PAPER_STYLE_SHEET="viz.mplstyle"
-PAPER_STYLE_SHEET_PATH=os.path.join(STYLE_SHEET_DIR,PAPER_STYLE_SHEET)
