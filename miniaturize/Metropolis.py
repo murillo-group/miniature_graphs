acefeb65b8c2677595bcf7c0e35514134f80139f
JAMO2001 <jorge_martinez3@baylor.edu>
2024-09-06 18:01:35 -0400

    Update Metropolis class to most recent version by David

--- miniaturize/Metropolis.py
+++ miniaturize/Metropolis.py
@@ -9,42 +9,46 @@ class Metropolis():
         self.graph = deepcopy(initial_graph)
         self.step = 0
 
-
-    def make_change(self):
+    def make_change(self, num_changes=1):
         '''
-        This function implements a change in a graph
+        This function implements a change in a graph\
         '''
         # make a deepcopy of the graph to ensure changes to temp_graph are independent of graph
         temp_graph = deepcopy(self.graph)
+        
+        # loop through the number of change you want to make
+        # note that it is possible that a change could be undone
+        # e.g. change one adds an edge then change two adds it back
+        for changes in range(num_changes):
+            non_edges = list(nx.non_edges(temp_graph))
+            edges = list(nx.edges(temp_graph))
 
-        non_edges = list(nx.non_edges(temp_graph))
-        edges = list(nx.edges(temp_graph))
+            # select a random edge that does not exist in the graph
+            new_node_one, new_node_two = non_edges[np.random.randint(len(non_edges))]
 
-        # select a random edge that does not exist in the graph
-        new_node_one, new_node_two = non_edges[np.random.randint(len(non_edges))]
-        # select a random edge that exists in the graph
-        old_node_one, old_node_two = edges[np.random.randint(len(edges))]
+            # select a random edge that exists in the graph
+            old_node_one, old_node_two = edges[np.random.randint(len(edges))]
 
-        # move an edge half of the time
-        if np.random.uniform(0,1) < .5:
-
-            # add the non-existant edge and remove the old edge. This is equal to moving an edge
-            temp_graph.add_edge(new_node_one, new_node_two)
-            temp_graph.remove_edge(old_node_one, old_node_two)
-
-        # add or remove an edge the other half of the time
-        else:
-            # add and edge half of the time
+            # move an edge half of the time
             if np.random.uniform(0,1) < .5:
 
-                # add a non-existant edge to the graph
+                # add the non-existant edge and remove the old edge. This is equal to moving an edge\
                 temp_graph.add_edge(new_node_one, new_node_two)
-
-            # remove an edge the other half of the time
-            else:
-
-                # remove an edge from the graph
                 temp_graph.remove_edge(old_node_one, old_node_two)
+
+            # add or remove an edge the other half of the time\
+            else:
+                # add and edge half of the time
+                if np.random.uniform(0,1) < .5:
+
+                    # add a non-existant edge to the graph
+                    temp_graph.add_edge(new_node_one, new_node_two)
+
+                # remove an edge the other half of the time
+                else:
+
+                    # remove an edge from the graph
+                    temp_graph.remove_edge(old_node_one, old_node_two)
 
         return temp_graph
 
@@ -52,11 +56,18 @@ class Metropolis():
         '''
         This function defines the loss function for the metropolis algorithm
         '''
-        graph_degree = (2*graph.number_of_edges()/graph.number_of_nodes()) / (graph.number_of_nodes()-1)
-        graph_assort = nx.degree_assortativity_coefficient(graph)/2
+        graph_degree = nx.density(graph)
+        graph_assort = nx.degree_assortativity_coefficient(graph)
         graph_clust = nx.average_clustering(graph)
+        
+        # max-min normalization\
+        graph_assort = (graph_assort+1)/2\
+        
 
-        energy = kwargs['degree_weight']*((graph_degree - kwargs['target_degree'])/kwargs['target_degree'])**2 + kwargs['assort_weight']*((graph_assort - kwargs['target_assort'])/kwargs['target_assort'])**2 + kwargs['clust_weight']*((graph_clust - kwargs['target_clust'])/kwargs['target_clust'])**2
+#         energy = kwargs['degree_weight']*((graph_degree_normalized - kwargs['target_degree'])/kwargs['target_degree'])**2 + kwargs['assort_weight']*((graph_assort_normalized - kwargs['target_assort'])/kwargs['target_assort'])**2 + kwargs['clust_weight']*((graph_clust_normalized - kwargs['target_clust'])/kwargs['target_clust'])**2
+        energy = kwargs['degree_weight']*abs((graph_degree - kwargs['target_degree'])) + \
+                 kwargs['assort_weight']*abs((graph_assort - kwargs['target_assort'])) + \
+                 kwargs['clust_weight']*abs((graph_clust - kwargs['target_clust']))
 
         return energy
 
@@ -70,10 +81,10 @@ class Metropolis():
         # if the if statement misses, return False
         return False
 
-    def run_step(self,targets_and_weights):
+    def run_step(self,targets_and_weights,num_changes=1):
 
         # change the graph
-        changed_graph = self.make_change()
+        changed_graph = self.make_change(num_changes)
 
         # check the energy of the current graph
         current_energy = self.energy_fnc(self.graph, **targets_and_weights)
@@ -89,7 +100,7 @@ class Metropolis():
             self.graph = deepcopy(changed_graph)
 
             # if not self.step % 1:
-                # print(f'Updated graph metrics\nAve. Degree: {2*self.graph.number_of_edges()/self.graph.number_of_nodes()}, Assortativity: {nx.degree_assortativity_coefficient(self.graph)}, Clustering: {nx.average_clustering(self.graph)}\n')
+                # print(f'Updated graph metrics\\nAve. Degree: \{2*self.graph.number_of_edges()/self.graph.number_of_nodes()\}, Assortativity: \{nx.degree_assortativity_coefficient(self.graph)\}, Clustering: {nx.average_clustering(self.graph)\}\\n')
 
 
         self.step += 1
@@ -104,21 +115,3 @@ class Metropolis():
     def get_energy(self, **kwargs):
 
         return self.energy_fnc(self.graph, **kwargs)
-
-
-
-
-# def test_switch_temperature(self, energy_1, energy_2, temp_1, temp_2):
-#     '''
-#     Parallel tempering check
-#     '''
-#     p = np.min( 1, np.exp( (energy_one - energy_two)*(1/temp_1 - 1/temp_2) ) )
-#
-#     if p > np.random.uniform(0,1):
-#         return True
-#
-#     return False
-#
-# def switch_temperature(self, other):
-#
-#     pass
