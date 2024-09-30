51d0fb1948b1f1e95a5bcb7b017891004309f937
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-09-30 17:41:00 -0400

    Initial calculate weights implementation

---
+++ src/calculate_weights.py
@@ -0,0 +1,37 @@
+from minigraphs import Metropolis
+import pandas as pd 
+import networkx as nx
+import sys
+'''Calculates the weights for the specified graph
+'''
+
+N = 1000
+density = 21.2e-3
+n_iterations = 10
+
+# Specify Metric funcions
+funcs_metrics = {
+    'density': nx.density,
+    'assortativity': lambda G: nx.degree_assortativity_coefficient(G)/2,
+    'clustering': nx.average_clustering
+}
+
+# Specify some metrics to 'match'
+metrics = {
+    'density' : 0,
+    'assortativity': 0,
+    'clustering': 0
+}
+
+# Explore the space
+replica = Metropolis(0,funcs_metrics,n_iterations)
+G = nx.erdos_renyi_graph(N,density)
+
+replica.transform(G,metrics,verbose=True)
+
+# Obtain the trajectories
+df = replica.trajectories_
+diff = df[['density','assortativity','clustering']].diff().abs()
+
+# Calculate the weights
+print(dict(1/diff.mean()))
