08a641ab448490d68baadae5d9474a7942b89c6c
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-10-02 22:08:18 -0400

    Increase time and memory in submit script

--- batch/miniaturize.sh
+++ batch/miniaturize.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #SBATCH --ntasks=6
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=1G
-#SBATCH --time=02:00:00
+#SBATCH --mem=7GB
+#SBATCH --time=08:00:00
 
 source ~/.profile_minigraphs
 
