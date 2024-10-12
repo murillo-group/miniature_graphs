96805e6de8fb2d30ca71e6639f8333c12d56e982
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-10-12 10:46:43 -0400

    Delete old submit scripts for miniaturize and parameters

--- research/mini_limits/simulate.sh
+++ research/mini_limits/simulate.sh
@@ -2,11 +2,11 @@
 #SBATCH --output=output_%A_%a.out
 #SBATCH --error=error_%A_%a.err
 #SBATCH --array=1-9
-#SBATCH --time=05:00:00  # Time limit
-#SBATCH --ntasks=6
-#SBATCH --mem=7G
+#SBATCH --time=01:00:00  # Time limit
+#SBATCH --mem=4G
 
 GRAPH_NAME=$1
+GRAPH_DIR="$DATA_DIR/$GRAPH_NAME/"
 
 # Source profile
 source ~/.profile_minigraphs
@@ -14,5 +14,6 @@ source ~/.profile_minigraphs
 # Calculate miniaturization size
 FRAC=$(echo "scale=3; $SLURM_ARRAY_TASK_ID * 0.1" | bc)
 
-# Run your Python script with the selected input file
-mpiexec -n 6 python -u "/mnt/home/martjor/repos/dev_pt/src/miniaturize.py" "$GRAPH_NAME" "$FRAC" 
+
+
+
