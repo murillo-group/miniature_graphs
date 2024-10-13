07b95d9c6ff2b04dd66ccd8bfdbbe6f6532c1c63
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-10-12 20:27:14 -0400

    Create batch submission template files for parameters and miniaturization

--- src/miniaturize_batch_template.sh
+++ src/miniaturize_batch_template.sh
@@ -2,9 +2,9 @@
 #SBATCH --job-name=minHamsterster
 #SBATCH --output=out_%x/output_%A_%a.out
 #SBATCH --error=out_%x/error_%A_%a.err
-#SBATCH --array=1-9
+#SBATCH --array=6-9
 #SBATCH --ntasks=6
-#SBATCH --time=5:00:00  # Time limit
+#SBATCH --time=15:00:00  # Time limit
 #SBATCH --mem=7G
 
 # Source profile
