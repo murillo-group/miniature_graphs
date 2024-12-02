a4625ee1b8a95b25b46fd47c84a4a5a9c57f8dde
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-12-01 19:14:19 -0500

    Update network plotting

--- workflows/scripts/graph_draw.py
+++ workflows/scripts/graph_draw.py
@@ -18,12 +18,27 @@ forceatlas2 = ForceAtlas2(gravity=1.0)
 
 positions = forceatlas2.forceatlas2_networkx_layout(graph,
                                                     pos=None,
-                                                    iterations=1000)
+                                                    iterations=100)
 
 # Draw Graph
-fig, ax = plt.subplots(figsize=(5,5),dpi=300)
-nx.draw_networkx_nodes(graph,positions,node_size=2,node_color="red",alpha=0.4)
-nx.draw_networkx_edges(graph,positions,edge_color="black",alpha=0.2)
-plt.axis('off')
+fig, ax = plt.subplots(figsize=(5,5),dpi=600)
+color = list(nx.clustering(graph).values())
+
+ax.set_title(f"|V|={graph.number_of_nodes()}, den={nx.density(graph):.2e}")
+ax.set_yticks([])
+ax.set_xticks([])
+ax.set_facecolor('black')
+
+nx.draw_networkx_nodes(graph,positions,
+                       node_size=0.1,
+                       node_color=color,
+                       vmin=0.0,
+                       vmax=1.0,
+                       alpha=0.7,
+                       cmap='cool')
+nx.draw_networkx_edges(graph,positions,
+                       edge_color="white",
+                       alpha=0.05,
+                       width=0.5)
 
 plt.savefig(drawing_file,bbox_inches='tight')
