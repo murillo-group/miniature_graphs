58e9c41a49042373b7ccac297af6e65420bc87be
Jorge Martinez-Ortiz <martjor@msu.edu>
2024-12-06 15:00:29 -0500

    Update params.py

--- src/params.py
+++ src/params.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
-import click
 from minigraphs import Metropolis
 from numpy import log
 import pandas as pd 
 import networkx as nx
 import sys
 import os
-import json
+import yaml
 '''Calculates the parameters for the specified graph
 '''
 
-@click.command()
-@click.argument('metrics_file',type=click.Path(exists=True))
-@click.argument('frac_size',type=click.INT)
-@click.option('--output_dir', default="", help='Output directory.')
-@click.option('--n_changes', default=10, help='Number of changes proposed at each iteration.')
-@click.option('--n_samples', default=10, help='Number of times parameters are calculated.')
-@click.option('--n_iterations', default=100, help='Number of iterations in each sample.')
 def params(metrics_file,
-           frac_size,
-           output_dir,
+           params_file,
+           shrinkage,
            n_changes,
            n_samples,
            n_iterations):
     
     # Retrieve Graph Metrics
     with open(metrics_file) as file:
-        metrics = json.load(file)
+        metrics = yaml.safe_load(file)
         metrics_target = {key:metrics[key] for key in ['density','assortativity_norm','clustering']}
 
     # Specify Metric funcions
@@ -38,7 +30,7 @@ def params(metrics_file,
 
     # Calculate miniature size
     try:
-        n_vertices = int(metrics['n_vertices'] * (frac_size/1000))
+        n_vertices = int(metrics['n_vertices'] * (1-shrinkage))
         
         if (n_vertices < 1):
             raise ValueError
@@ -47,7 +39,7 @@ def params(metrics_file,
         print("Error: Invalid number of vertices in the miniature")
     
     print(f"Calculating parameters for graph at graph at {metrics_file}")
-    print(f"\t - Size: {n_vertices} nodes ({frac_size/10}% miniaturization)")
+    print(f"\t - Size: {n_vertices} nodes ({shrinkage:.02f}% miniaturization)")
     print(f"\t - Number iterations per sample: {n_iterations}")
     print(f"\t - Number of samples: {n_samples}\n")
 
@@ -55,35 +47,38 @@ def params(metrics_file,
     params = []
     for i in range(n_samples):
         print(f"Sweep {i+1}/{n_samples}")
-        replica = Metropolis(0,
-                            funcs_metrics,
-                            n_iterations=n_iterations,
-                            n_changes=n_changes
-                            )
         
+        # Construct replica
+        replica = MH(funcs_metrics,
+                     schedule=lambda beta: 0
+                     n_changes=n_changes)
+        
+        # Transform ER graph
         G = nx.erdos_renyi_graph(n_vertices,metrics_target['density'])
-        replica.transform(G,metrics_target)
+        replica.transform(G,
+                          metrics_target
+                          n_iterations=n_iterations)
 
-        df = replica.trajectories_.copy()
-        
-        print(df)
+        # Retrieve trajectories
+        df = replica.trajectories_
         weights = dict(1/df[replica.metrics].diff().abs().mean())
-
+        
         print("Weights:")
-        print(json.dumps(weights,indent=4))
+        print(weights)
 
         # Calculate optimal beta
-        replica = Metropolis(0,
-                            funcs_metrics,
-                            n_iterations=n_iterations,
-                            metrics_weights=weights,
-                            n_changes=n_changes
-                            )
+        replica = MH(funcs_metrics,
+                     schedule=lambda beta:0,
+                     n_changes=n_changes,
+                     weights=weights)
         
+        # Transform ER graph
         G = nx.erdos_renyi_graph(n_vertices,metrics_target['density'])
-        replica.transform(G,metrics_target)
+        replica.transform(G,
+                          metrics_target
+                          n_iterations=n_iterations)
 
-        df = replica.trajectories_.copy()
+        df = replica.trajectories_
 
         beta = -log(0.23) * 1/df['Energy'].diff().abs().mean()
 
@@ -99,10 +94,6 @@ def params(metrics_file,
     print("Final parameters:")
     print(params)
 
-    # Save to JSON
-    file_name = os.path.join(output_dir,f'params_{frac_size:03d}.json')
-    with open(file_name,'w+') as f:
-        json.dump(dict(params),f,indent=4)
-        
-if __name__ == '__main__':
-    params()
+    # Save to yaml
+    with open(params_file,'w') as file:
+        yaml.dump(params,file,default_flow_style=False)
